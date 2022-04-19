f48195aedc6e31b7e9eb33180d56c5768b53def2
GorgeousMooseNipple <orang768@gmail.com>
2022-04-19 21:01:18 +0300

    Rename cache folder

--- app/src/main/java/lab/android/audiodementia/client/CacheManager.java
+++ app/src/main/java/lab/android/audiodementia/client/CacheManager.java
@@ -12,8 +12,8 @@ public class CacheManager {
     public static void enableHttpCaching(Context appContext) {
         try {
             File httpCacheDir = new File(appContext.getCacheDir()
-                    , "http");
-            long httpCacheSize = 20 * 1024 * 1024; // 10 MiB
+                    , "httpCache");
+            long httpCacheSize = 20 * 1024 * 1024;
             HttpResponseCache.install(httpCacheDir, httpCacheSize);
         } catch (IOException e) {
             Log.i("CACHING_FAILED"
