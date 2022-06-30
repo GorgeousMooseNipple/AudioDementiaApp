ddd1cf19be818a0b255205c98c77d9289a99502e
GorgeousMooseNipple <orang768@gmail.com>
2022-06-30 20:33:27 +0300

    Add status code to HttpResponse classes

--- app/src/main/java/lab/android/audiodementia/client/HttpResponse.java
+++ app/src/main/java/lab/android/audiodementia/client/HttpResponse.java
@@ -1,11 +1,13 @@
 package lab.android.audiodementia.client;
+import java.net.HttpURLConnection;
 
 public class HttpResponse {
 
     private boolean successful;
+    private int statusCode;
     private String message;
 
-    HttpResponse(boolean successful, String message) {
+    HttpResponse(boolean successful, int statusCode, String message) {
         this.successful = successful;
         this.message = message;
     }
@@ -16,5 +18,9 @@ public class HttpResponse {
 
     public String getMessage() {
         return message;
+    }
+
+    public boolean isUnauthorized() {
+        return this.statusCode == HttpURLConnection.HTTP_UNAUTHORIZED;
     }
 }
